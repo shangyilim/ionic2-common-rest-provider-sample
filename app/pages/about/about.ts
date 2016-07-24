2c09eebf32fc510c9b43bb1621702d4f6dcba89e
shangyi <shangyilim@gmail.com>
2016-07-24 09:14:07 +0800

    separate into another page to demonstrate concept more clearly

--- app/pages/about/about.ts
+++ app/pages/about/about.ts
@@ -1,10 +1,22 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
+import { PostsProvider } from '../../providers/posts-provider/posts-provider'
+import { Post } from '../../typings/post';
 
 @Component({
-  templateUrl: 'build/pages/about/about.html'
+  templateUrl: 'build/pages/about/about.html',
+  providers: [PostsProvider]
 })
 export class AboutPage {
-  constructor(private navController: NavController) {
+  
+  posts: Post[];
+  
+  constructor(private navController: NavController, private postsProvider: PostsProvider) {
+  }
+
+  getPosts(){
+    this.postsProvider.get().then((response: Post[])=>{
+      this.posts = response;
+    });
   }
 }
